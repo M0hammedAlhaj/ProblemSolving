fb8f8ec494a28a378bb9f649e445e7cd27092f69
MohammedMamounHamedAlhaj <134335033+M0hammedAlhaj@users.noreply.github.com>
2025-03-16 04:54:24 +0300

    Solved

--- reverseInt/src/main/java/Solution.java
+++ reverseInt/src/main/java/Solution.java
@@ -1,34 +1,16 @@
-import static java.lang.Integer.*;
-
 class Solution {
-
-    public static int reverse(int x) {
-
-        StringBuilder s;
+    public int reverse(int x) {
+        int res = 0;
         boolean isNegative = x < 0;
-        Long r;
-        if (isNegative) {
-            if (Long.parseLong(String.valueOf(x)) * -1 > MAX_VALUE) {
-                return 0;
-            }
-            x *= -1;
-            s = new StringBuilder(String.valueOf(x));
-            s.reverse();
-            r = Long.parseLong(s.toString()) * -1;
-            return r < MIN_VALUE ? 0 : parseInt(s.toString()) * -1;
+        String strX = String.valueOf(Math.abs(x));
+        StringBuilder sb = new StringBuilder(strX).reverse();
+
+        try {
+            res = Integer.parseInt(sb.toString());
+        } catch (NumberFormatException e) {
+            return 0;
         }
-        s = new StringBuilder(String.valueOf(x));
-        s.reverse();
-        r = Long.parseLong(s.toString());
-        return r > MAX_VALUE ? 0 : parseInt(s.toString());
-    }
 
-    public static void main(String[] args) {
-        System.out.println(reverse(-2147483648));
-        System.out.println(reverse(-652));
-        System.out.println(reverse(0));
-        System.out.println(reverse(12));
-
-
+        return isNegative ? -res : res;
     }
 }
