ceebb633c3a5a9ac70b96691572e2f39d5cbbe2d
MohammedAlhaj <mohamedalhaj900@gmail.com>
2025-06-18 22:48:19 +0300

    SOLVED

---
+++ DailyTemperatures/untitled/src/main/java/org/example/Main.java
@@ -0,0 +1,30 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] res = new int[n];
+
+        for (int i = n - 2; i >= 0; i--) {
+            int j = i + 1;
+            while (j < n && temperatures[j] <= temperatures[i]) {
+                if (res[j] == 0) {
+                    j = n;
+                    break;
+                }
+                j += res[j];
+            }
+
+            if (j < n) {
+                res[i] = j - i;
+            }
+        }
+        return res;
+    }
+}
