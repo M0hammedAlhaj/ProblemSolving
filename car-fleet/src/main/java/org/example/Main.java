066a1c34aa5162402cbaf55e80d4feb94a4c70c9
MohammedAlhaj <mohamedalhaj900@gmail.com>
2025-06-21 13:40:16 +0300

    SOLVED O(n) no sort and stack

--- car-fleet/src/main/java/org/example/Main.java
+++ car-fleet/src/main/java/org/example/Main.java
@@ -7,6 +7,8 @@ import java.util.Stack;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+
+        CarFleet(12, new int[]{10,8,0,5,3}, new int[]{2,4,1,1,3});
     }
 
     public int carFleet(int target, int[] position, int[] speed) {
@@ -20,12 +22,29 @@ public class Main {
         for (int[] p : pair) {
             stack.push((double) (target - p[0]) / p[1]);
             if (stack.size() >= 2 &&
-                    stack.peek() <= stack.get(stack.size() - 2))
-            {
+                    stack.peek() <= stack.get(stack.size() - 2)) {
                 stack.pop();
             }
         }
         return stack.size();
+    }
+
+    public static int CarFleet(int target, int[] position, int[] speed) {
+        var tt = new double[target];
+
+        for (int i = 0; i < position.length; ++i)
+            tt[position[i]] = (double) (target - position[i]) / speed[i];
+
+        int output = 0;
+        double low = 0.0;
+        for (int i = target - 1; i >= 0; --i) {
+            if (tt[i] > low) {
+                ++output;
+                low = tt[i];
+            }
+        }
+
+        return output;
     }
 
 }
